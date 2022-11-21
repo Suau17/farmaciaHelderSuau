ba9ed31025b26a2ccb28b8bdcdd36362d0f7e077
HelderRivas <Helder24102003@gmail.com>
2022-11-21 13:17:28 +0100

    migrations

--- database/migrations/2022_11_21_115821_create_productes_table.php
+++ database/migrations/2022_11_21_115821_create_productes_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('productes', function (Blueprint $table) {
             $table->id();
+            $table->integer('prod_ID');
+            $table->string('nom');
+            $table->string('tipus');
             $table->timestamps();
         });
     }
--- database/migrations/2022_11_21_115841_create_treballadors_table.php
+++ database/migrations/2022_11_21_115841_create_treballadors_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('treballadors', function (Blueprint $table) {
             $table->id();
+            $table->string('dni');
+            $table->string('name');
+            $table->set('gender',['male','female']);
             $table->timestamps();
         });
     }
